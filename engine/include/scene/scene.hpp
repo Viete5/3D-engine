10e4a4fea44029bda1b18ce012c733447aeaa985
Viete5 <damirfathutdinov634@gmail.com>
2026-04-26 13:17:47 +0300

    add scene class

---
+++ engine/include/scene/scene.hpp
@@ -0,0 +1,40 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include <vector>
+#include "sceneObject.hpp"
+#include "camera.hpp"
+#include "light.hpp"
+
+namespace engine::scene {
+
+class Scene {
+public:
+    Scene(const std::vector<SceneObject>& objects, const Camera& camera, const std::vector<Light>& lights);
+    Scene(const Scene& other) = delete;
+    Scene& operator=(const Scene& other) = delete;
+    virtual ~Scene() = default;
+
+    virtual void update(float delta_time_value) = 0;
+
+    std::vector<SceneObject>& get_objects();
+    Camera& get_camera();
+    std::vector<Light>& get_lights();
+    
+    const std::vector<SceneObject>& get_objects() const;
+    const Camera& get_camera() const;
+    const std::vector<Light>& get_lights() const;
+
+    void set_objects(const std::vector<SceneObject>& new_objects);
+    void set_camera(const Camera& new_camera);
+    void set_lights(const std::vector<Light>& new_lights);
+
+protected:
+    std::vector<SceneObject> objects;
+    Camera camera;
+    std::vector<Light> lights;
+};
+
+}
+
+#endif // SCENE_HPP
---
+++ engine/src/scene/scene.cpp
@@ -0,0 +1,47 @@
+#include "../include/scene/scene.hpp"
+
+namespace engine::scene {
+
+Scene::Scene(const std::vector<SceneObject>& objects, const Camera& camera, const std::vector<Light>& lights) 
+    : objects(objects),
+      camera(camera),
+      lights(lights) {}
+
+const std::vector<SceneObject>& Scene::get_objects() const {
+    return objects;
+}
+
+std::vector<SceneObject>& Scene::get_objects() {
+    return objects;
+}
+
+const Camera& Scene::get_camera() const {
+    return camera;
+}
+
+Camera& Scene::get_camera() {
+    return camera;
+}
+
+const std::vector<Light>& Scene::get_lights() const {
+    return lights;
+}
+
+std::vector<Light>& Scene::get_lights() {
+    return lights;
+}
+
+void Scene::set_objects(const std::vector<SceneObject>& new_objects) {
+    objects = new_objects;
+}
+
+void Scene::set_camera(const Camera& new_camera) {
+    camera = new_camera;
+}
+
+void Scene::set_lights(const std::vector<Light>& new_lights) {
+    lights = new_lights;
+}
+
+
+} // namespace engine::scene
